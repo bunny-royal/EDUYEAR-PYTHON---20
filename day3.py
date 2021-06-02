b4dc69ba04da1160e4e559657d3712a242c2f5c7
bunny-royal <84795178+bunny-royal@users.noreply.github.com>
2021-06-02 13:16:44 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,21 @@
+
+## age caluculator
+
+
+birthyear=int(input('enter birth year: '))
+age=2021-birthyear
+print ("age is {}".format(age))
+
+
+
+
+## Simple caluculator
+a=int(input('enter first  number : '))
+b=int(input('enter second number: '))
+print("addition is {}".format(a+b))
+print("substraction is {}".format(a-b))
+print("multiplication is {}".format(a*b))
+print("floor division is {}".format(a/b))
+print("decimal division is {}".format(a//b))
+print("remainder is {}".format(a%b))
+print("power of input numbers is {}".format(a**b))
