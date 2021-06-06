3c810338c1831fc871384466f7f9e8ce5e23bef9
bunny-royal <84795178+bunny-royal@users.noreply.github.com>
2021-06-06 19:54:38 +0530

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,21 @@
+#### occurence of y in input string given by user 
+
+a=input('enter any string : ')
+occurence= a.count('y')
+print("count of y is {}".format(occurence))
+
+
+#### printing even indexed charecters
+b=str(input('enter any string : '))
+print(b[2:7:2])
+
+
+
+#### using swapcase function
+c='EdUyEaR'
+output=c.swapcase()
+print("output is {}".format(output))
+
+
+
+
