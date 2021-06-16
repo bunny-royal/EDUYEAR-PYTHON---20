9167e27019d5d2fadabe0a9c130250870ed1817b
bunny-royal <84795178+bunny-royal@users.noreply.github.com>
2021-06-16 19:00:53 +0530

    Delete day6(4).py

--- day6(4).py
+++
@@ -1,9 +0,0 @@
-a=[0,1,2,9,7,6,4,96,101]
-even_count,odd_count=0,0
-for i in a:
-	if i%2==0:
-		even_count +=1
-	else:
-		odd_count +=1
-		print('Even numbers count is {}'.format(even_count))
-		print('odd numbers count is {}'.format(odd_count))
